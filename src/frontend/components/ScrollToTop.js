12297f23c86454b68e6e2ade1301ef665c011529
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-06-25 15:00:18 +0500

    scroll to top at some particular routes

---
+++ src/frontend/components/ScrollToTop.js
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+const scrollToTopRoutes = [
+  "/",
+  "/about",
+  "/cart",
+  "/all-dish-detail",
+  "/become-a-chef",
+  "/faqs",
+  "/checkout",
+  "/order-summary",
+  "/privacy-policy",
+  "/terms-of-servies",
+  "/homemade-food-delivery",
+]; // Routes to scroll to top
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const shouldScrollToTop = scrollToTopRoutes.includes(pathname);
+
+    if (shouldScrollToTop) {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
+
+  return null; // No need to render any UI
+};
+
+export default ScrollToTop;
