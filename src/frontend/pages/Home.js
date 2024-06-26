95c73ae7aacefc7fbbdb60b5fbc6516aeb23b641
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-06-27 01:16:26 +0500

    Region modal added

--- src/frontend/pages/Home.js
+++ src/frontend/pages/Home.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import HeroBanner from '../components/homeWidget/HeroBanner'
@@ -10,23 +10,62 @@ import CommunityStories from '../components/homeWidget/CommunityStories'
 import Subscriber from '../components/homeWidget/Subscriber'
 import BrowsByCategories from '../components/homeWidget/BrowsByCategories'
 import PopularCollection from '../components/homeWidget/PopularCollection'
+import { useSelector } from 'react-redux'
+import RegionDropdown from '../components/RegionDropdown'
+import { toast } from 'react-toastify'
 
 export const Home = () => {
+    const [ regionAvailable, setRegionAvailable ] = useState(localStorage.getItem("region") ? true : false);
+  
+    //Modal Region Selection
+    const OnSelectRegion = () =>{
+      if(localStorage.getItem('region')){
+        setRegionAvailable(true);
+        window.location.reload();
+      } 
+      else {
+        console.log("Please select region")
+        toast.dismiss();
+        toast.error("Please select the region")
+      }
+    }
+
+    const { userInfo } = useSelector(state => state.user);
+
     return (
-        <>
-            <Header />
-                <HeroBanner/>
-                <BrowsByCategories/>
-                <PopularCollection/>
-                {/* <LocalChef/> */}
-                <MostLoveChef/>
-                <MeetChef/>
-                <CustomerReviews/>
-                <CommunityStories/>
-                <Subscriber/>
-                
-            <Footer />
-        </>
-    )
+      <>
+        <div
+          className={
+            !regionAvailable
+              ? "fixed top-0 bottom-0 h-[100vh] w-[100vw] px-4 z-30 bg-[#121212c0] flex justify-center items-center"
+              : "hidden"
+          }
+        >
+          {/* Modal */}
+          <div className="bg-white min-w-[200px] w-[100%] max-w-[350px] p-2 py-4 rounded shadow-sm flex flex-col gap-2 ">
+            <h2 className="text-base font-semibold">Select Region</h2>
+            <RegionDropdown />
+            <button
+              onClick={OnSelectRegion}
+              className="bg-primary text-white p-1 px-2 rounded w-max"
+            >
+              Submit
+            </button>
+          </div>
+        </div>
+        <Header />
+        {!userInfo && <HeroBanner />}
+        <BrowsByCategories />
+        <PopularCollection />
+        {/* <LocalChef/> */}
+        <MostLoveChef />
+        <MeetChef />
+        <CustomerReviews />
+        <CommunityStories />
+        <Subscriber />
+
+        <Footer />
+      </>
+    );
 }
 export default Home
