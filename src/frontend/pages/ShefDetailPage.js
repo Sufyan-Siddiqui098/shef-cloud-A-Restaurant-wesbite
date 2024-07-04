9e25e89d536a9574e77f92749ea1a484274e2257
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-07-05 02:30:42 +0500

    Api call for ChefWithDishes

--- src/frontend/pages/ShefDetailPage.js
+++ src/frontend/pages/ShefDetailPage.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 import ChefContent from '../components/chefDetailWidgets/ChefContent'
 import FilterAndDate from '../components/chefDetailWidgets/FilterAndDate'
+import { handleGetChefWithDishes } from '../../services/get_without_auth'
+import { useParams } from 'react-router-dom'
 
 export const ShefDetailPage = () => {
+
+    const {chefId} = useParams();
+    const [chefAndDishes, setChefAndDishes] = useState([]);
+    // Get chef with dishes
+    useEffect(()=> { 
+        (async()=>{
+            try { 
+                const city = JSON.parse( localStorage.getItem('region') );
+                const response = await handleGetChefWithDishes(chefId, city.id);
+                console.log("response of chef with dishes ", response);
+                setChefAndDishes(response)
+
+            } catch(error) {
+                console.error(error)
+            }
+        }) ()
+    }, [chefId])
+    // console.log(chefAndDishes)
     return (
         <>
             <Header />
             <div className='shefDetail_page'>
-                <ChefContent />
-                <FilterAndDate />
+                <ChefContent chefAndDishes={chefAndDishes} />
+                <FilterAndDate chefAndDishes={chefAndDishes} />
             </div>
             <Footer />
         </>
