7ca7dcf6bdc78d11803088fc9343c86045366120
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-07-07 18:53:19 +0500

    New pages added

--- src/App.js
+++ src/App.js
@@ -27,6 +27,10 @@ import AuthProtected from "./protected_route/AuthProtected";
 import ShefProtected from "./protected_route/ShefProtected";
 import ScrollToTop from "./frontend/components/ScrollToTop";
 import CategorizeDishes from "./frontend/pages/CategorizeDishes";
+import AllChef from "./frontend/pages/AllChef";
+import ShefCoupon from "./shef_dashboard/pages/ShefCoupon";
+import UserOrder from "./frontend/pages/UserOrder";
+import ShefOrderSummary from "./shef_dashboard/pages/ShefOrderSummary";
 
 function App() {
   useEffect(() => {
@@ -59,19 +63,21 @@ function App() {
       <Route path="/login" element={<SignIn />} />
       <Route path="/register" element={<SignUp />} />
       <Route path="/become-a-chef" element={<BecomeChef />} />
-      <Route path="/shef-detail" element={<ShefDetailPage />} />
-      <Route path="/all-dish-detail/:foodTypeId?" element={<AllDishDetail />} />
-      <Route path="/dish-detail-single/:dishId?" element={<DishDetailSingle />} />
+      <Route path="/shef-detail/:chefId?" element={<ShefDetailPage />} />
+      <Route path="/all-dish-detail" element={<AllDishDetail />} />
+      <Route path="/dish-detail-single/:dishId" element={<DishDetailSingle />} />
       <Route path="/terms-of-servies" element={<TermsOfServices />} />
       <Route path="/privacy-policy" element={<PrivacyPolicy />} />
       <Route path="/homemade-food-delivery" element={<HomeFoodDelivery />} />
       <Route path="/faqs" element={<Faqs />} />
       <Route path="/categorize-dishes/:foodCategoryId?" element={<CategorizeDishes />} />
+      <Route path="/all-chef" element={<AllChef />} />
       {/* Protected Route */}
       <Route element={<AuthProtected/>}>
         <Route path="/cart" element={<CartPage />} />
-        <Route path="/checkout" element={<Checkout />} />
+        <Route path="/checkout/:chefId" element={<Checkout />} />
         <Route path="/order-summary" element={<OrderSummary />} />
+        <Route path="/order" element={<UserOrder />} />
       </Route>
 
       {/* SHEF DASHBOARD ROUTES */}
@@ -82,6 +88,8 @@ function App() {
         <Route path="/shef/order" element={<Order />} />
         <Route path="/shef/sales-statment" element={<SalesStament />} />
         <Route path="/shef/order-review" element={<OrderReview />} />
+        <Route path="/shef/coupon" element={<ShefCoupon />} />
+        <Route path="/shef/order-summary" element={<ShefOrderSummary />} />
       </Route>
 
     </Routes>
