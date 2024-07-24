50f8b9eec0e3bbac1fc974ea73618d464c5c37ca
shahs50 <shahnoor.khan@hotmail.com>
2024-07-24 20:22:53 +0100

    UserOrder without search

--- src/shef_dashboard/pages/Order.js
+++ src/shef_dashboard/pages/Order.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../shef_dashboard/components/Header'
 import { Link } from 'react-router-dom'
+import { handleChangeOrderStatus, handleGetOrders } from '../../services/order'
+import { useSelector } from 'react-redux'
+import { toast } from 'react-toastify'
+
 export const Order = () => {
+
+    const { authToken } = useSelector((state) => state.user)
+    const [orderDetails, setOrderDetails] = useState([]);
+
+    useEffect(() => {
+        const fetchOrders = async () => {
+            try {
+                const ordersRetrieved = await handleGetOrders(authToken)
+                setOrderDetails(ordersRetrieved);
+            } catch (error) {
+                console.log("Error While Fetching Orders \n", error)
+            }
+        };
+
+        fetchOrders();
+        console.log("useEffect is running userorder");
+    }, [authToken]);
+
+    // Function to handle search input change
+    const handleOnChange = (e) => {
+        const searchTerm = e.target.value.toLowerCase();
+        // const items = dropdownMenuRef.current.querySelectorAll("option");
+        // items.forEach((item) => {
+        // const text = item.innerText.toLowerCase();
+        // if (text.includes(searchTerm)) {
+        //     item.style.display = "block";
+        // } else {
+        //     item.style.display = "none";
+        // }
+        // });
+    };
+
+    // Function to handle status change
+    const handleStatusChange = (e,id) => {
+        const newStatus = e.target?.value?.toLowerCase();
+        const saveStatus = async () => {
+            try {
+                if(newStatus){
+                    const ordersRetrieved = await handleChangeOrderStatus(authToken,{status:newStatus},id)
+                    console.log(ordersRetrieved)
+                    if(ordersRetrieved.success){
+                        toast('Order Status Updated!')
+                    }
+                }
+                // setOrderDetails(ordersRetrieved);
+            } catch (error) {
+                console.log("Error While Fetching Orders \n", error)
+            }
+        };
+        saveStatus();
+    };
     return (
         <>
             <div className=''>
@@ -9,16 +64,16 @@ export const Order = () => {
                 <div className='container mx-auto p-5'>
                     <div className='mt-6 p-5 bg-white rounded-xl border border-borderClr'>
                         <div className=''>
-                            <h3 className='text-xl font-semibold leading-tight pt-2 mb-3'>My Orders for <span>Friday, February 23, 2024 (EST)</span></h3>
+                            <h3 className='text-xl font-semibold leading-tight pt-2 mb-3'>My Orders for <span>{new Date().toLocaleDateString('en-US', { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}</span></h3>
                         </div>
-                        <div className='grid grid-cols-12 gap-3'>
+                            <div className='grid grid-cols-12 gap-3'>
                             <div className='lg:col-span-4 md:col-span-6 col-span-12'>
                                 <input type='date' className='w-full' />
                             </div>
                             <div className='lg:col-span-4 md:col-span-6 col-span-12 lg:block hidden'></div>
                             <div className='lg:col-span-4 md:col-span-6 col-span-12'>
                                 <div className='flex relative'>
-                                    <input className='w-full' placeholder='Search' />
+                                    <input className='w-full' placeholder='Search' onChange={handleOnChange} />
                                     <div className='absolute right-2 top-[13px]'>
                                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="18" height="18" fill="#323232">
                                             <path d="M11 2C15.968 2 20 6.032 20 11C20 15.968 15.968 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2ZM11 18C14.8675 18 18 14.8675 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18ZM19.4853 18.0711L22.3137 20.8995L20.8995 22.3137L18.0711 19.4853L19.4853 18.0711Z"></path>
@@ -39,34 +94,37 @@ export const Order = () => {
                                             <th className='w-[15%]'>Quantity</th>
                                             <th className='w-[15%]'>Spice Level</th>
                                             <th className='w-[15%]'>Portion Size</th>
-                                            <th className='w-[15%]'>Serving Size</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        <tr className='border-b'>
+                                    {orderDetails
+                                        // .filter(filteredOrder => filteredOrder.status === 'pending')
+                                        .map(order =>
+                                        order.order_details?.map(detail => (
+                                            <tr className='border-b' key={detail.id}>
                                             <td>
-                                                <p className='text-primaryGreen text-[14px]'>#ID03456</p>
+                                                <p className='text-primaryGreen text-[14px]'>{order.order_code}</p>
                                             </td>
                                             <td>
-                                                {/* <h4 className='text-[14px] mb-0 leading-tight font-semibold'>Fish Cutlus</h4> */}
                                                 <Link to='/shef/order-summary' className='text-[14px] mb-0 leading-tight font-semibold'>
-                                                Fish Cutlus
+                                                {detail.name}
                                                 </Link>
                                             </td>
                                             <td>
-                                                <h4 className='text-[14px] mb-0 leading-tight'>x2</h4>
+                                                <h4 className='text-[14px] mb-0 leading-tight'>x{detail.quantity}</h4>
                                             </td>
                                             <td>
-                                                <h4 className='text-[14px] mb-0 leading-tight'>Mild</h4>
+                                                <h4 className='text-[14px] mb-0 leading-tight'>{detail.user_menu?.spice_level?.name}</h4>{/*spice level id is only being sent*/}
                                             </td>
                                             <td>
-                                                <h4 className='text-[14px] mb-0 leading-tight'>16 grams</h4>
+                                                <h4 className='text-[14px] mb-0 leading-tight'>{detail.user_menu?.portion_size} grams</h4>
                                             </td>
-                                            <td>
-                                                <h4 className='text-[14px] mb-0 leading-tight'>3 Serving</h4>
-                                            </td>
-                                        </tr>
+                                            </tr>
+                                        ))
+                                        )
+                                    }
                                     </tbody>
+
                                 </table>
                             </div>
                             <div >
@@ -78,52 +136,66 @@ export const Order = () => {
                                                 <th className='w-[10%]' >Order ID</th>
                                                 <th className='w-[16%]' >Dish Name</th>
                                                 <th className='w-[15%]'>Delivery Date/Day/Time</th>
-                                                <th className='w-[10%]'>Customer</th>
-                                                <th className='w-[9%]'>Zip Code</th>
+                                                <th className='w-[13%]'>Customer</th>
+                                                {/* <th className='w-[9%]'>Zip Code</th> */}
                                                 <th className='w-[8%]'>Quantity</th>
-                                                <th className='w-[10%]'>Your Tips</th>
-                                                <th className='w-[10%]'>Total Earnin</th>
-                                                <th className='w-[22%]'>Action</th>
+                                                <th className='w-[12%]'>Chef Earning</th>
+                                                <th className='w-[12%]'>Total Price</th>
+                                                <th className='w-[14%]'>Action</th>
                                             </tr>
                                         </thead>
                                         <tbody>
+                                        {orderDetails
+                                        // .filter(filteredOrder => filteredOrder.status === 'pending')
+                                        .map(order =>
+                                        order.order_details?.map(detail => (
                                             <tr className='border-b'>
                                                 <td>
-                                                    <p className='text-primaryGreen text-[14px] mb-0'>#ID03456</p>
+                                                    <p className='text-primaryGreen text-[14px] mb-0'>{order.order_code}</p>
                                                 </td>
                                                 <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>Fish Cutlus</h4>
+                                                    <h4 className='text-[14px] mb-0 leading-tight'>{detail.name}</h4>
                                                 </td>
                                                 <td>
-                                                    <h4 className='text-[14px] mb-1 leading-tight'>- 20/2/2024</h4>
-                                                    <h4 className='text-[14px] mb-1 leading-tight'>- Monday</h4>
-                                                    <h4 className='text-[14px] mb-1 leading-tight'>- 2:00am</h4>
+                                                    <h4 className='text-[14px] mb-1 leading-tight'>- {new Date(order.delivery_time).toLocaleDateString()}</h4>
+                                                    <h4 className='text-[14px] mb-1 leading-tight'>- {new Date(order.delivery_time).toLocaleDateString('en-US', { weekday: 'long' })}</h4>
+                                                    <h4 className='text-[14px] mb-1 leading-tight'>- {new Date(order.delivery_time).toLocaleTimeString()}</h4>
                                                 </td>
                                                 <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>David Miler</h4>
+                                                    <h4 className='text-[14px] mb-0 leading-tight'>{order.name}</h4>
+                                                </td>
+                                                {/* <td>
+                                                    <h4 className='text-[14px] mb-0 leading-tight'>{order.zip_code || 'N/A'}</h4>
+                                                </td> */}
+                                                <td>
+                                                    <h4 className='text-[14px] mb-0 leading-tight'>x{detail.quantity}</h4>
                                                 </td>
                                                 <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>20001</h4>
+                                                    <h4 className='text-[14px] mb-0 leading-tight'>{order.chef_earning_price.toLocaleString("en-PK", {
+                                                        style: "currency",
+                                                        currency: "PKR",
+                                                        })}
+                                                    </h4>
                                                 </td>
                                                 <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>x2</h4>
+                                                    <h4 className='text-[14px] mb-0 leading-tight'>{order.total_price.toLocaleString("en-PK", {
+                                                        style: "currency",
+                                                        currency: "PKR",
+                                                        })}
+                                                    </h4>
                                                 </td>
                                                 <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>$5.50</h4>
-                                                </td>
-                                                <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>$30.00</h4>
-                                                </td>
-                                                <td>
-                                                    <select id="selectOption">
-                                                        <option value="">Active</option>
-                                                        <option value="option1">Deactive</option>
-                                                        <option value="option2">Pending</option>
-                                                        <option value="option3">Prepairing</option>
-                                                    </select>
-
+                                                <select id="selectOption" defaultValue={order.status} onChange={(e)=> handleStatusChange(e,order.id)}>
+                                                    <option value="pending">Pending</option>
+                                                    <option value="accepted">In Process</option>
+                                                    <option value="delivered">Delivered</option>
+                                                    <option value="cancelled">Cancelled</option>
+                                                </select>
                                                 </td>
                                             </tr>
+                                            ))
+                                            )
+                                        }
                                         </tbody>
                                     </table>
                                 </div>
