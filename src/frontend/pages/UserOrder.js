7c77a2369a09a9df2dcca60fec59610e8416dab3
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-08-13 21:31:55 +0500

    review and rating modal added with api calling

--- src/frontend/pages/UserOrder.js
+++ src/frontend/pages/UserOrder.js
@@ -1,56 +1,93 @@
-import React, { useEffect, useState } from 'react'
-import Header from '../components/Header'
-import { Link } from 'react-router-dom'
-import { handleGetOrders } from '../../services/order'
-import { useSelector } from 'react-redux'
+import React, { useEffect, useState } from "react";
+import Header from "../components/Header";
+import { Link } from "react-router-dom";
+import { handleGetOrders, handleOrderReviewAndRating } from "../../services/order";
+import { useSelector } from "react-redux";
+import Modal from "react-modal";
+import { toast } from "react-toastify";
 
 const UserOrder = () => {
+  const { authToken } = useSelector((state) => state.user);
+  // const orderDetailInitial = {
+  //     order_id: 1,
+  //     dish_name: '',
+  //     quantity: '',
+  //     spice_level: '',
+  //     portion_size: '',
+  //     serving_size: '',
+  // };
+  const [orderDetails, setOrderDetails] = useState([]);
 
-    const { authToken } = useSelector((state) => state.user)
-    // const orderDetailInitial = {
-    //     order_id: 1,
-    //     dish_name: '',
-    //     quantity: '',
-    //     spice_level: '',
-    //     portion_size: '',
-    //     serving_size: '',
-    // };
-    const [orderDetails, setOrderDetails] = useState([]);
+  useEffect(() => {
+    const fetchOrders = async () => {
+      try {
+        const ordersRetrieved = await handleGetOrders(authToken);
+        // console.log("user/shef orders", ordersRetrieved);
+        // Flatten the nested order details into a single array of order details
+        const mappedOrderDetails = ordersRetrieved.reduce((acc, order) => {
+          const orderDetails = order.order_details.map((detail) => ({
+            order_id: order.order_code,
+            id: order.id,
+            dish_name: detail.name,
+            quantity: detail.quantity,
+            spice_level: detail.user_menu.spice_level?.name,
+            portion_size: detail.user_menu.portion_size,
+            reviews: order?.reviews,
+            // serving_size: detail.user_menu.portion_type_id,//name is not available at this time  - removed
+          }));
+          return acc.concat(orderDetails);
+        }, []);
 
-    useEffect(() => {
-        const fetchOrders = async () => {
-            try {
-                const ordersRetrieved = await handleGetOrders(authToken)
-                console.log(ordersRetrieved)
-                // Flatten the nested order details into a single array of order details
-                const mappedOrderDetails = ordersRetrieved.reduce((acc, order) => {
-                    const orderDetails = order.order_details.map(detail => ({
-                        order_id: order.order_code,
-                        dish_name: detail.name,
-                        quantity: detail.quantity,
-                        spice_level: detail.user_menu.spice_level?.name,
-                        portion_size: detail.user_menu.portion_size,
-                        // serving_size: detail.user_menu.portion_type_id,//name is not available at this time  - removed
-                    }));
-                    return acc.concat(orderDetails);
-                }, []);
+        setOrderDetails(mappedOrderDetails);
+      } catch (error) {
+        console.log("Error While Fetching Orders \n", error);
+      }
+    };
 
-                setOrderDetails(mappedOrderDetails);
-            } catch (error) {
-                console.log("Error While Fetching Orders \n", error)
-            }
-        };
+    fetchOrders();
+    console.log("useEffect is running userorder");
+  }, [authToken]);
 
-        fetchOrders();
-        console.log("useEffect is running userorder");
-    }, [authToken]);
-    return (
-        <>
-            <div className=''>
-                <Header />
-                <div className='container mx-auto p-5'>
-                    <div className='mt-6 p-5 bg-white rounded-xl border border-borderClr'>
-                        {/* <div className=''>
+  //--- reviews & rating
+  const [reviewAndRating, setReviewAndRating] = useState({
+    order_id: null,
+    rating: 0,
+    review: "",
+    order_code: null
+  });
+  const [isOpen, setIsOpen] = useState(false);
+  const onRequestClose = () => {
+    setIsOpen(false);
+    setReviewAndRating({order_id: null, review: "", rating: 0, order_code: null})
+  };
+
+  // Rating & review submit
+  const ratingSubmit = async() => { 
+    try {
+        const payload = {};
+        payload.order_id = reviewAndRating.order_id;
+        payload.rating = reviewAndRating.rating;
+        if(reviewAndRating.review)
+            payload.review = reviewAndRating.review
+        console.log("Paylod is ", payload)
+        const respons = await handleOrderReviewAndRating(authToken, payload);
+        console.log("order reviews and rating ", respons)
+        // setReviewAndRating({order_id: null, review: "", rating: 0})
+        toast.success("Rating Submitted")
+        onRequestClose();
+    } catch (error) {
+        console.log("SOmething went wrong while rating order \n", error.message)
+        toast.error(error.message || "Something went wrong while Rating")
+    }
+  }
+
+  return (
+    <>
+      <div className="">
+        <Header />
+        <div className="container mx-auto p-5">
+          <div className="mt-6 p-5 bg-white rounded-xl border border-borderClr">
+            {/* <div className=''>
                             <h3 className='text-xl font-semibold leading-tight pt-2 mb-3'>My Orders for <span>Friday, February 23, 2024 (EST)</span></h3>
                         </div>
                         <div className='grid grid-cols-12 gap-3'>
@@ -70,22 +107,24 @@ const UserOrder = () => {
                             </div>
                         </div> */}
 
-                        <div>
-                            <h3 className='text-xl font-semibold leading-tight uppercase pt-2 mb-3 mt-6 border-b pb-2'>Orders</h3>
-                            <div className='overflow-x-auto'>
-                                <table className="text-left w-full menuTable border-0">
-                                    <thead>
-                                        <tr className='border-b'>
-                                            <th className='w-[20%]'>Order ID</th>
-                                            <th className='w-[20%]'>Dish Name</th>
-                                            <th className='w-[15%]'>Quantity</th>
-                                            <th className='w-[15%]'>Spice Level</th>
-                                            <th className='w-[15%]'>Portion Size</th>
-                                            {/* <th className='w-[15%]'>Serving Size</th> */}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {/* <tr className='border-b'>
+            <div>
+              <h3 className="text-xl font-semibold leading-tight uppercase pt-2 mb-3 mt-6 border-b pb-2">
+                Orders
+              </h3>
+              <div className="overflow-x-auto">
+                <table className="text-left w-full menuTable border-0">
+                  <thead>
+                    <tr className="border-b">
+                      <th className="w-[20%]">Order ID</th>
+                      <th className="w-[20%]">Dish Name</th>
+                      <th className="w-[15%]">Quantity</th>
+                      <th className="w-[15%]">Spice Level</th>
+                      <th className="w-[15%]">Portion Size</th>
+                      {/* <th className='w-[15%]'>Serving Size</th> */}
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {/* <tr className='border-b'>
                                             <td>
                                                 <p className='text-primaryGreen text-[14px]'>#ID03456</p>
                                             </td>
@@ -107,34 +146,52 @@ const UserOrder = () => {
                                                 <h4 className='text-[14px] mb-0 leading-tight'>3 Serving</h4>
                                             </td>
                                         </tr> */}
-                                        {orderDetails.map((detail, index) => (
-                                            <tr key={index} className='border-b'>
-                                                <td>
-                                                    <p className='text-primaryGreen text-[14px]'>#{detail.order_id}</p>
-                                                </td>
-                                                <td>
-                                                    <Link to='/order-summary' className='text-[14px] mb-0 leading-tight font-semibold'>
-                                                        {detail.dish_name}
-                                                    </Link>
-                                                </td>
-                                                <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>x{detail.quantity}</h4>
-                                                </td>
-                                                <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>{detail.spice_level}</h4>
-                                                </td>
-                                                <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>{detail.portion_size}</h4>
-                                                </td>
-                                                {/* <td>
-                                                    <h4 className='text-[14px] mb-0 leading-tight'>{detail.serving_size}</h4>
-                                                </td> */}
-                                            </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
-                            </div>
-                            {/* <div >
+                    {orderDetails.map((detail, index) => (
+                      <tr key={index} className="border-b">
+                        <td>
+                          <p className="text-primaryGreen text-[14px]">
+                            #{detail.order_id}
+                          </p>
+                        </td>
+                        <td>
+                          <Link
+                            to="/order-summary"
+                            className="text-[14px] mb-0 leading-tight font-semibold"
+                          >
+                            {detail.dish_name}
+                          </Link>
+                        </td>
+                        <td>
+                          <h4 className="text-[14px] mb-0 leading-tight">
+                            x{detail.quantity}
+                          </h4>
+                        </td>
+                        <td>
+                          <h4 className="text-[14px] mb-0 leading-tight">
+                            {detail.spice_level}
+                          </h4>
+                        </td>
+                        <td>
+                          <h4 className="text-[14px] mb-0 leading-tight">
+                            {detail.portion_size}
+                          </h4>
+                        </td>
+                        {/* 
+                          <td>
+                              <h4 className='text-[14px] mb-0 leading-tight'>{detail.serving_size}</h4>
+                          </td> 
+                        */}
+                        {detail?.reviews?.length<1 && <td>
+                          <button onClick={()=> { setReviewAndRating(prev =>{ return{...prev, "order_id": detail.id, order_code: detail.order_id} } ); setIsOpen(true) }} className="text-[14px] text-primary hover:underline focus:underline mb-0 leading-tight">
+                            Add Rating
+                          </button>
+                        </td>}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+              {/* <div >
                                 <h3 className='text-xl font-semibold leading-tight uppercase pt-2 mb-3 mt-6 border-b pb-2'>Active Orders(1)</h3>
                                 <div className='overflow-x-auto'>
                                     <table className="text-left w-full menuTable border-0">
@@ -193,9 +250,9 @@ const UserOrder = () => {
                                     </table>
                                 </div>
                             </div> */}
-                        </div>
-                    </div >
-                    {/* <div className='mt-6 p-5 bg-white rounded-xl border border-borderClr'>
+            </div>
+          </div>
+          {/* <div className='mt-6 p-5 bg-white rounded-xl border border-borderClr'>
                         <div >
                             <h3 className='text-xl font-semibold leading-tight uppercase mb-3 border-b pb-2'>Refunded Order (0)</h3>
                             <div className='overflow-x-auto'>
@@ -237,10 +294,97 @@ const UserOrder = () => {
                             </div>
                         </div>
                     </div > */}
-                </div >
-            </div >
-        </>
-    )
-}
+        </div>
+      </div>
 
-export default UserOrder
+      <Modal
+        isOpen={isOpen}
+        onRequestClose={onRequestClose}
+        contentLabel="Reviews"
+      >
+        <div className="flex flex-col  h-ful">
+          {/* Modal content here */}
+          <div className="flex items-center justify-between border-b pb-3 gap-3">
+            <h2 className="text-lg font-semibold leading-tight mb-0">
+              Order Review & Rating
+            </h2>
+            <button onClick={onRequestClose}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                width="24"
+                height="24"
+                fill="rgba(0,0,0,1)"
+              >
+                <path d="M11.9997 10.5865L16.9495 5.63672L18.3637 7.05093L13.4139 12.0007L18.3637 16.9504L16.9495 18.3646L11.9997 13.4149L7.04996 18.3646L5.63574 16.9504L10.5855 12.0007L5.63574 7.05093L7.04996 5.63672L11.9997 10.5865Z"></path>
+              </svg>
+            </button>
+          </div>
+
+          <div>
+            {/* Order Code  */}
+            <p className="my-4 text-primaryGreen "><strong>Order Code :</strong> #{reviewAndRating.order_code}</p>
+
+            <h3 className="mb-3 text-lg text-gray my-2 font-semibold">
+              Add Reviews
+            </h3>
+            <textarea
+              name=""
+              id=""
+              className="py-2 min-h-20"
+              placeholder="Write Review Here"
+              maxLength={500}
+              value={reviewAndRating.review || ""}
+              onChange={(e) =>setReviewAndRating((prev) => {
+                return { ...prev, review: e.target.value };
+              })}
+            />
+            {/* Rating */}
+            <div className="mt-3 mb-5">
+              <h5 className="my-1 text-lg text-gray font-semibold">
+                Rating : ( {reviewAndRating.rating} )
+              </h5>
+              <div className="flex gap-1">
+                {[...Array(5)].map((_, index) => {
+                  const currentRate = index + 1;
+                  return (
+                    <label key={currentRate}>
+                      <input
+                        type="radio"
+                        name="rating"
+                        className="hidden"
+                        value={currentRate}
+                        onClick={() =>
+                          setReviewAndRating((prev) => {
+                            return { ...prev, rating: currentRate };
+                          })
+                        }
+                      />
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        width="18"
+                        height="18"
+                        fill={
+                          currentRate <= reviewAndRating.rating ? "#FF9529" : ""
+                        }
+                      >
+                        <path d="M12.0006 18.26L4.94715 22.2082L6.52248 14.2799L0.587891 8.7918L8.61493 7.84006L12.0006 0.5L15.3862 7.84006L23.4132 8.7918L17.4787 14.2799L19.054 22.2082L12.0006 18.26Z"></path>
+                      </svg>
+                    </label>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+
+          <button onClick={ratingSubmit} className="my-6 bg-primary text-white text-lg w-full uppercase px-6 py-2 font-semibold rounded-lg disabled:opacity-60 mt-auto">
+            Submit
+          </button>
+        </div>
+      </Modal>
+    </>
+  );
+};
+
+export default UserOrder;
