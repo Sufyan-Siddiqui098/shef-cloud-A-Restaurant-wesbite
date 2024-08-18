cf22df301c8a68917debfc9aad7f85b700a09bf7
Sufyan Siddiqui <134385064+Sufyan-Siddiqui098@users.noreply.github.com>
2024-08-18 14:20:42 +0500

    removed the defaultSetting

--- src/services/order.js
+++ src/services/order.js
@@ -36,17 +36,7 @@ export const handleGetOrders = async (token) => {
     console.error("Error While fetching orders\n", error);
   }
 };
-// ALready in the file "default_setting" - Remove the below one after getting it from "default_setting "file 
-export const handleDefaultSettings = async (token) => {
-  try {
-    const { data } = await api.get("/api/default_settings", {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-    return data;
-  } catch (error) {
-    console.error("Error While fetching orders\n", error);
-  }
-};
+
 export const handleChangeOrderStatus = async (token, payload, id) => {
   try {
     // console.log(token, payload, id);
