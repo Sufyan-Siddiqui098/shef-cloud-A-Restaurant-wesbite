fbe51ebc1eb5384562351bd75dae76d8d66d4bcc
shahs50 <shahnoor.khan@hotmail.com>
2024-10-01 17:28:08 +0100

    axios updated for deployment

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const baseURL = process.env.REACT_APP_baseURL || "http://shef-admin.test";
+const baseURL = process.env.REACT_APP_baseURL || "http://backend-shef.exactomark.com";
 
 const api = axios.create({baseURL});
 
