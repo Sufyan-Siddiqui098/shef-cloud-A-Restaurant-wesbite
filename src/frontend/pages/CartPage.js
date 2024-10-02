baa909ec1a4d2bbfba7a0611ef954b9944190459
Sufyan Siddiqui <sufyansiddiqui098@gmail.com>
2024-10-02 16:37:16 -0700

    delivery slot is in 12 hr format with suffix

--- src/frontend/pages/CartPage.js
+++ src/frontend/pages/CartPage.js
@@ -4,6 +4,7 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import { useSelector } from "react-redux";
 import isValidURL from "../../ValidateUrl";
+import convertTo12Hour from "../../convertTo12Hours";
 
 export const CartPage = () => {
   const { cartItem } = useSelector((state) => state.cart);
@@ -31,8 +32,8 @@ export const CartPage = () => {
                 <path d="M4.00488 16V4H2.00488V2H5.00488C5.55717 2 6.00488 2.44772 6.00488 3V15H18.4433L20.4433 7H8.00488V5H21.7241C22.2764 5 22.7241 5.44772 22.7241 6C22.7241 6.08176 22.7141 6.16322 22.6942 6.24254L20.1942 16.2425C20.083 16.6877 19.683 17 19.2241 17H5.00488C4.4526 17 4.00488 16.5523 4.00488 16ZM6.00488 23C4.90031 23 4.00488 22.1046 4.00488 21C4.00488 19.8954 4.90031 19 6.00488 19C7.10945 19 8.00488 19.8954 8.00488 21C8.00488 22.1046 7.10945 23 6.00488 23ZM18.0049 23C16.9003 23 16.0049 22.1046 16.0049 21C16.0049 19.8954 16.9003 19 18.0049 19C19.1095 19 20.0049 19.8954 20.0049 21C20.0049 22.1046 19.1095 23 18.0049 23Z"></path>
               </svg>
               <span className="text-white text-xl font-semibold"> */}
-                {/* View Cart */}
-                {/* Checkout All
+        {/* View Cart */}
+        {/* Checkout All
               </span>
             </Link>
           </div>
@@ -111,10 +112,22 @@ export const CartPage = () => {
                         {/* <h3 className='mb-1 text-lg font-semibold leading-tight'>Guajillo Grilled Shrimps </h3> */}
                         <h3 className="mb-1 text-lg font-semibold leading-tight">
                           {menu.name}
-                        <div className="flex flex-col">
-                        <p className="text-xs my-0 text-headGray mt-[2px] tracking-wider">Date: {new Date(chef.delivery_date).toDateString()}</p>
-                        <p className="text-xs my-0 text-headGray tracking-wider">Slot: {chef.delivery_slot}</p>
-                        </div>
+                          <div className="flex flex-col">
+                            <p className="text-xs my-0 text-headGray mt-[2px] tracking-wider">
+                              Date:{" "}
+                              {new Date(chef.delivery_date).toDateString()}
+                            </p>
+                            <p className="text-[10px] mt-1 xl:text-xs my-0 text-headGray tracking-wider">
+                              Slot:{" "}
+                              {convertTo12Hour(
+                                chef.delivery_slot?.split("-")[0]
+                              ) +
+                                " - " +
+                                convertTo12Hour(
+                                  chef.delivery_slot?.split("-")[1]
+                                )}
+                            </p>
+                          </div>
                         </h3>
                       </div>
                       {/* <h6 className='text-lg font-semibold leading-tight w-[20%] text-right mb-0 pr-2'>1x</h6> */}
